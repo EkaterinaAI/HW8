3f8447c4cedda184a8ed94f3d41aa04752668897
zvereva <zverewaekaterina@gmail.com>
2023-01-19 20:19:56 +0400

    Внесла изменения после просмотра видео

--- HW8/Task1/src/Main.java
+++ HW8/Task1/src/Main.java
@@ -1,9 +1,9 @@
+import java.util.Arrays;
 import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        //#1 Создать массив из 10 целых чисел. Значения могут быть любыми
-        //todo
+        //#1 Создать массив из 10 целых чисел. Значения могут быть любыми\
 
         int array[] = new int[5];
 
@@ -11,32 +11,36 @@ public class Main {
             array[i] = new Random().nextInt(1000);
         }
 
-        //#2 Распечатать все значения массива начиная с 0 до последнего индексов.
-        //todo
+        //#2 Распечатать все значения массива начиная с 0 до последнего индексов.\
 
         for (int i = 0; i < array.length; i++) {
             System.out.println(array[i]);
         }
-        //#3 Распечатать все значения массива с конца (начиная с последнего до 0 индексов).
-        //todo
+        //#3 Распечатать все значения массива с конца (начиная с последнего до 0 индексов).\
 
         /*for (int i = array.length - 1; i >= 0; i--) {
             System.out.println(array[i]);
         }*/
-        //#4 *** Задание особой сложности. Развернуть массив(см конец урока).
-        //todo
+        //#4 *** Задание особой сложности. Развернуть массив(см конец урока).\
 
         System.out.println("--------------------");
 
-        int yarra[] = new int[5];
+        /*int yarra[] = new int[5];
 
         for (int i = array.length - 1; i >= 0; i--) {
-            int value = array[i];
             for (int j = 0; j < yarra.length; j++) {
-                yarra[j] = value;
+                yarra[j] = array[i];
                 System.out.println(yarra[j]);
                 break;
             }
+        }*/
+        for (int i = 0; i < array.length / 2; i++) {
+            int temp = array[i];
+            array[i] = array[array.length - (1 + i)];
+            array[array.length - (1 + i)] = temp;
+        }
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
         }
     }
 }
--- HW8/Task2/src/Main.java
+++ HW8/Task2/src/Main.java
@@ -3,20 +3,13 @@ import java.util.Random;
 public class Main {
     public static void main(String[] args) {
         //todo код программы писать тут
-        int weight[] = new int[10];
+        double weight[] = new double[10];
         double summ = 0;
         int countPeople = 0;
 
         for (int i = 0; i < weight.length; i++) {
-            weight[i] = new Random().nextInt(40, 100);
-        }
-
-        for (int i = 0; i < weight.length; i++) {
-            System.out.println(weight[i]);
+            weight[i] = new Random().nextDouble(40, 100);
             summ += weight[i];
-        }
-
-        for (int i = 0; i < weight.length; i++) {
             if (weight[i] >= 60 && weight[i] <= 80) {
                 countPeople++;
             }
